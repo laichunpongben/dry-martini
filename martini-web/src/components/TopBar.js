d9780b78c5313974a98cfce438ae84a35e1a24d0
Ben Lai <laichunpongben@gmail.com>
2025-06-12 00:57:55 +0800

    Remove About link from TopBar

--- martini-web/src/components/TopBar.js
+++ martini-web/src/components/TopBar.js
@@ -3,25 +3,13 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 
 export default function TopBar() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar variant="dense">
-        {/* Spacer to push "About" to the right */}
+        {/* Spacer to maintain layout after removing About link */}
         <Box sx={{ flexGrow: 1 }} />
-
-        <Link
-          href="https://about.databookman.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          color="inherit"
-          underline="none"
-          sx={{ fontWeight: 500 }}
-        >
-          About
-        </Link>
       </Toolbar>
     </AppBar>
   );
